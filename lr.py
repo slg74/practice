684ffef6231848a14972ba3bcbf3c398ced77e44
slg74 <scottghome@scottgs-MBP.lan>
2024-09-13 17:50:37 -0500

    reverse list practice

---
+++ lr.py
@@ -0,0 +1,51 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+a = Node('A')
+b = Node('B')
+c = Node('C')
+d = Node('D')
+e = Node('E')
+
+a.next = b
+b.next = c
+c.next = d
+d.next = e
+
+def print_linked_list(head):
+    cur = head
+    while cur != None:
+        print(cur.val)
+        cur = cur.next
+
+print_linked_list(a)
+print('\n')
+
+
+#    None  ->  A  ->   B    ->   C  ->   D  ->  E
+#    prev     cur      next
+#
+#    None  ->  A  ->   B    ->   C  ->   D  ->  E
+#             prev     cur       next
+#           
+#    None  ->  A  ->   B    ->  C  ->   D  ->  E
+#                      prev     cur     next
+
+def reverse_linked_list(head):       
+    cur = head
+    prev = None
+    while cur != None:
+        next = cur.next
+        cur.next = prev
+        prev = cur
+        cur = next
+    return prev
+
+print_linked_list(reverse_linked_list(a))
+
+
+
+
+
