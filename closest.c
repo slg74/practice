448ddbdf63f119cc3819869d35cd6998be28e50c
Scott Gillespie <scott.layne.gillespie@gmail.com>
2024-09-23 08:18:00 -0400

    Update closest.c
    Remove rewrite practice fxn

--- closest.c
+++ closest.c
@@ -15,22 +15,6 @@ int findClosestNumber(int* nums, int numSize) {
     return closest;
 }
 
-int find_closest(int* nums, int numSize) {
-    int closest = nums[0];
-    for (int i=1; i<numSize; i++) {
-        if (abs(nums[i]) < abs(closest)) {
-            closest = nums[i];
-        }
-    }
-
-    for (int i=0; i<numSize; i++) {
-        if (abs(nums[i]) == closest) {
-            return abs(nums[i]); 
-        }
-    }
-    return closest; 
-}
-
 int main() {
     int ar[] = { -4, -2, 3, 4, 8 };
     int ret = findClosestNumber(ar, sizeof(ar) / sizeof(ar[0])); 
