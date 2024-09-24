1f077fa00dab316c45d0d5daca78b0181cdc1a8c
slg74 <scottghome@scottgs-MBP.lan>
2024-09-24 08:45:00 -0500

    a little better but still lots of forearm tattoos and one speed bikes

--- merge_alternately.rs
+++ merge_alternately.rs
@@ -2,21 +2,30 @@ fn merge_alternately(word1: &str, word2: &str) -> String {
     let mut result = String::with_capacity(word1.len() + word2.len());
     let mut chars1 = word1.chars();
     let mut chars2 = word2.chars();
+    let mut i = 0;
+    let mut j = 0;
 
-    while let (Some(c1), Some(c2)) = (chars1.next(), chars2.next()) {
-        result.push(c1);
-        result.push(c2);
+    while i < word1.len() || j < word2.len() {
+        if i < word1.len() {
+            if let Some(c) = chars1.next() {
+                result.push(c);
+            }
+            i += 1;
+        }
+        if j < word2.len() {
+            if let Some(c) = chars2.next() {
+                result.push(c);
+            }
+            j += 1;
+        }
     }
-
-    result.extend(chars1);
-    result.extend(chars2);
 
     result
 }
 
 fn main() {
     let word1 = "abc";
-    let word2 = "pqr";
+    let word2 = "pqrs";
     let result = merge_alternately(word1, word2);
     println!("Merged string: {}", result);
 }
