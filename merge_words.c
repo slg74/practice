f7bb181a04f44d4cd68e806ae56e9e932bc1b624
slg74 <scottghome@scottgs-MBP.lan>
2024-09-24 08:34:41 -0500

    lets do this in rust

--- merge_words.c
+++ merge_words.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-char* mergeAlternately(char* word1, char* word2) {
+char *mergeAlternately(char* word1, char* word2) {
     int len1 = strlen(word1);
     int len2 = strlen(word2); 
     int merged_len = len1 + len2;
@@ -22,12 +22,13 @@ char* mergeAlternately(char* word1, char* word2) {
         }
     }
     merged[k] = '\0';  // end the merged string
+
     return merged;
 }
 
 int main() {
-    char word1[] = "abc";
-    char word2[] = "pqr";
+    char word1[] = "abcdf";
+    char word2[] = "pqrzx";
     char* result = mergeAlternately(word1, word2);
     printf("Merged string: %s\n", result);
     free(result);
