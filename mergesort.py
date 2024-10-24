f63b406336190601af1a0086ee4481968dd9a99f
Scott Gillespie <scott.layne.gillespie@gmail.com>
2024-10-23 21:54:28 -0500

    Update mergesort.py

--- mergesort.py
+++ mergesort.py
@@ -1,25 +1,17 @@
 def merge_sort(arr):
-    # Return if the array has 1 or no elements
     if len(arr) < 2:
         return
 
-    # Finding the middle of the array
     mid = len(arr) // 2
 
-    # Dividing the array elements into 2 halves
     left_half = arr[:mid]
     right_half = arr[mid:]
 
-    # Sorting the first half
     merge_sort(left_half)
-
-    # Sorting the second half
     merge_sort(right_half)
 
-    # Merging the two halves
     i = j = k = 0
-
-    # Copy data to temp arrays L[] and R[]
+    
     while i < len(left_half) and j < len(right_half):
         if left_half[i] < right_half[j]:
             arr[k] = left_half[i]
@@ -29,7 +21,6 @@ def merge_sort(arr):
             j += 1
         k += 1
 
-    # Checking if any element was left
     while i < len(left_half):
         arr[k] = left_half[i]
         i += 1
@@ -40,7 +31,7 @@ def merge_sort(arr):
         j += 1
         k += 1
 
-# Example usage
+
 arr = [38, 27, 43, 3, 9, 82, 10]
 print("Original array:", arr)
 merge_sort(arr)
